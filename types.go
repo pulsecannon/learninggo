72fd46d496e0783a00be90ff07dceb6abaeca796
Pranav Hegde <mphegde@google.com>
2020-06-11 12:36:14 -0700

    Types.

---
+++ types.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+// func add(x, y float32) float32 {
+// 	return x + y
+// }
+
+// func multiple(a, b string) (string, string) {
+// 	return a, b
+// }
+
+func main() {
+	// num1, num2 := 5.6, 9.5
+
+	// fmt.Println(add(num1, num2))
+
+	// w1, w2 := "Hey", "There"
+	// fmt.Println(multiple(w1, w2))
+
+	var a int = 62
+	var b float64 = float64(a)
+
+	x := a // x will be of type a.
+}
